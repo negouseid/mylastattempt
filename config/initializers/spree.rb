787db80ec82ac8d7502f9ba3e4e913f1dd2ae308
negouseid <negouseid@gmail.com>
2015-11-10 01:50:02 -0700

    update gems

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -2,36 +2,53 @@
 # See http://docs.solidus.io/Spree/AppConfiguration.html for details
 
 Spree.config do |config|
+  # Without this preferences are loaded and persisted to the database. This
+  # changes them to be stored in memory.
+  # This will be the default in a future version.
+  config.use_static_preferences!
 
-   attachment_config = {
+  # Core:
 
-   s3_credentials: {
-     access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-     secret_access_key: ENV['AWS_SECRET_KEY_ID'],
-     bucket:            ENV['S3_BUCKET']
-   },
+  # Default currency for new sites
+  config.currency = "USD"
 
-   storage:        :s3,
-   s3_headers:     { "Cache-Control" => "max-age=31557600" },
-   s3_protocol:    "https",
-   bucket:         ENV['S3_BUCKET_NAME'],
-   url:            "https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=mikesmovies&prefix=",
+  # from address for transactional emails
+  config.mails_from = "store@example.com"
 
-   styles: {
-       mini:     "48x48>",
-       small:    "100x100>",
-       product:  "240x240>",
-       large:    "600x600>"
-   },
+  # Uncomment to stop tracking inventory levels in the application
+  # config.track_inventory_levels = false
 
-   path:           "/:class/:id/:style/:basename.:extension",
-   default_url:    "/:class/:id/:style/:basename.:extension",
-   default_style:  "product"
- }
+  # When true, product caches are only invalidated when they come in or out of
+  # stock. Default is to invalidate cache on any inventory changes.
+  # config.binary_inventory_cache = true
 
- attachment_config.each do |key, value|
-   Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
- end
+
+  # Frontend:
+
+  # Custom logo for the frontend
+  # config.logo = "logo/solidus_logo.png"
+
+  # Template to use when rendering layout
+  # config.layout = "spree/layouts/spree_application"
+
+
+  # Admin:
+
+  # Custom logo for the admin
+  # config.admin_interface_logo = "logo/solidus_logo.png"
+
+  # Gateway credentials can be configured statically here and referenced from
+  # the admin. They can also be fully configured from the admin.
+  #
+  # config.static_model_preferences.add(
+  #   Spree::Gateway::StripeGateway,
+  #   'stripe_env_credentials',
+  #   secret_key: ENV['STRIPE_SECRET_KEY'],
+  #   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  #   server: Rails.env.production? ? 'production' : 'test',
+  #   test: !Rails.env.production?
+  # )
+  
 
 end
 
