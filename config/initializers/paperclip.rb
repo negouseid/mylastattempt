287a6f402859101f3332931e46ecd78e60c01a31
negouseid <negouseid@gmail.com>
2015-11-10 03:09:04 -0700

    update paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
 Paperclip::Attachment.default_options[:url] = 'https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=mikesmovies&prefix='
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
