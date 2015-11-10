bc8a3bb2321298c56ef5e348361e691633d30a6b
negouseid <negouseid@gmail.com>
2015-11-10 03:02:56 -0700

    add paperclip

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,2 @@
+Paperclip::Attachment.default_options[:url] = 'https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=mikesmovies&prefix='
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
